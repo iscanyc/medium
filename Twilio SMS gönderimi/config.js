c37255405e57c5eb5f4f2a78e1af38ca1fc117a8
Can <42694838+iscanyc@users.noreply.github.com>
2021-03-25 22:51:31 +0300

    Create config.js

---
+++ Twilio SMS gönderimi/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    accountSid: "", //Twilio Console sayfasındaki ACCOUNT SID.
+    authToken: "", //Twilio Console sayfasındaki AUTH TOKEN.
+    twilioNumber: "", //Twilio'dan kiraladığınız numara.
+    myNumber: "" //Mesajın gönderileceği numara, örnek: +905555555555
+}
+
+//Twilio Console: https://i.imgur.com/LynIwmw.png
